d38e2998325d857a13ad44ae2354656f5bb22f29
ShitalBhise <bhiseshital27059@gmail.com>
2022-02-20 16:08:33 +0530

    Added: Simulate Stopwatch program logic

---
+++ src/com/logic/Stopwatch.java
@@ -0,0 +1,32 @@
+package com.logic;
+
+import java.util.Scanner;
+
+public class Stopwatch {
+	public static double startWatch() {
+		System.out.println("Enter 1 to start watch");
+		try (Scanner scanner = new Scanner(System.in)) {
+			double startWatch = scanner.nextDouble();
+			startWatch = System.currentTimeMillis();
+			return startWatch;
+		}
+
+	}
+
+	public static double stopWatch() {
+		System.out.println("Enter 0 to stop watch");
+		try (Scanner scanner = new Scanner(System.in)) {
+			double stopWatch = scanner.nextDouble();
+			stopWatch = System.currentTimeMillis();
+			return stopWatch;
+		}
+
+	}
+
+	public static void main(String[] args) {
+		double timeInSec = Math.abs((startWatch() - stopWatch())) / 1000;
+		System.out.println("time in sec is " + timeInSec);
+
+	}
+
+}
